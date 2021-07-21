e530fb7f20bd18eaacf54477a88c7788300b0b1f
Huzaifa Mohammed <huzaifa.coder785@gmail.com>
2021-07-21 01:10:14 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let getBooks = async () => {
+    let booksRresponse = await fetch("https://www.anapioficeandfire.com/api/books/?pageSize=50")
+    let booksData = await booksRresponse.json()
+    // console.log(booksData)
+    let booksContainer = document.createElement("div")
+    booksContainer.className = "container"
+    booksData.forEach(book => {
+        let booksList = document.createElement("div")
+        booksList.className = "card"
+        booksList.innerHTML = 
+                        `<h1 class="heading">📕 ${book.name}</h1>
+                        <p>🔢 ISBN: ${book.isbn}</p>
+                        <p>📃 Pages: ${book.numberOfPages}</p>
+                        <p>📝Author/Authors: ${book.authors}</p>
+                        <p>🤵 Publisher Name: ${book.publisher}</p>
+                        <p>⏰ Released On: ${book.released}</p>
+                        <p>Character Names Will Come Here...</p>
+                        
+                        `
+        document.body.append(booksContainer)
+        booksContainer.append(booksList)
+    });
+}
+getBooks();
